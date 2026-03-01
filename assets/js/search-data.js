bd1714f90b312aedea4812cb8a1013883195e217
runqiuw <105184852+runqiuw@users.noreply.github.com>
2026-03-01 05:28:28 +0000

    Deploying to gh-pages from @ runqiuw/runqiuw.github.io@a8988031f70fb2b71b17169e9a4dda806f7dcd72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-portfolio",
-          title: "Portfolio",
+          title: "portfolio",
           description: "",
           section: "Navigation",
           handler: () => {
